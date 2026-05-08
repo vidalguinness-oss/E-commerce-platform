252f76a8b8f498df837a6cac55cfdaa148635d54
vidal guinness <vidalguinness@gmail.com>
2026-05-08 17:58:06 +0300

    Add JWT authentication settings to settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+}
